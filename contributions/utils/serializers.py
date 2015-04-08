ea0982a86c9c03bf1de5f5dbefba649386667aee
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-04-08 10:05:17 -0700

    add case for is None

--- contributions/utils/serializers.py
+++ contributions/utils/serializers.py
@@ -6,10 +6,13 @@ from datetime import datetime, date, time
 class JSONEncoder(json.JSONEncoder):
 
     def default(self, o):
+        print o
         if isinstance(o, ndb.Key):
             o = ndb.Key.get(o)
         if isinstance(o, ndb.Model):
             return dict(ndb.Model.to_dict(o), **dict(id=o.key.id()))
         elif isinstance(o, (datetime, date, time)):
             return str(o)
+        elif o is None:
+            return "null"
         return json.JSONEncoder.default(self, o)
