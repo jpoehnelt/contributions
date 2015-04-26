bd06ee0df63c66f50a58a865a06e80304df4ab72
Michael Gilbert <drumz4jah@yahoo.com>
2015-04-25 21:02:05 -0700

    [unfinished] Got the collecting closer...
    Still needs some tweaking. The information is not yet being stored in
    the database. Running the file it will grab all repos and display a
    summary as it goes

--- build/collect_data.py
+++ build/collect_data.py
@@ -10,7 +10,7 @@ PROJECT_API_URL = 'http://contributions-907.appspot.com/api/project'
 STUDENT_API_URL = 'http://contributions-907.appspot.com/api/student'
 COMMIT_API_URL = 'http://contributions-907.appspot.com/api/commit'
 
-
+ALL_REPOS_COMMITS_URLS = []
 GITHUB_REPOS = [
     #band
     ['Adam-Thomas/Band-Proj1','alecdavidson/joyful-waves','AustinAbhari/CS399_TheBand','c1phr/cs399_band','cmh553/theBand','DylanGrayson/CS399_TheBand','itj3/bandPage','justinwp/cs399-band','kbullins/CS399Band','lf237/band','lmk243/BandProject','sk367/cs399_band'],
@@ -25,7 +25,9 @@ GITHUB_REPOS = [
 ]
 DUE_DATE_MAPPING = ["2015-01-23", "2015-02-06", "2015-02-20", "2015-04-03"]
 
-'''
+##### TESTING - just grap one project ######
+#GITHUB_REPOS = [['EduardoIniguez/CS399-final']]
+
 def get_repo(repo_name, project_number):
     print "Getting Repo: %s" % repo_name
 
@@ -39,22 +41,26 @@ def get_repo(repo_name, project_number):
 
     # error handling for posting project
 
-    if response.status_code == 200:
+    if response.status_code == 200 or response.status_code == 500:
         pass
     elif response.status_code == 409:
         # already exists
         response = requests.get(PROJECT_API_URL + "/%s" % repo_name, auth=('cs399contributions', 'contributions399'))
     else:
         raise Exception()
+	
+	#################################################################
+	# TODO actually put the project information in the datastore
+	#################################################################
+	# now get the object we just posted
+    #response = requests.get(PROJECT_API_URL + "/%s" % repo_name, auth=('cs399contributions', 'contributions399'))
+	#project = json.loads(response.text)
+	
+	
+	# get all of the commits and contributors from the commits
+    total_commits = parse_commits(data)
 
-    project = response.json
-
-    # get all of the commits and contributors from the commits
-    parse_commits(project)
-
-    print "Found %d Commits" % project['commit_count']
-
-    print "Saving Number of Commits to Project"
+    print "'Fake' Saving %d Commits to Project" % total_commits 
     update = {
         "project_number": project_number,
         "repo_name": repo_name
@@ -68,89 +74,108 @@ def save_repo(data):
 
 
 def parse_commits(project):
-    """
-    Gets all commits for the repo
-    :param project:
-    :return: None
-    """
-    project['commit_count'] = 0
-    print "Getting Commits for: %s" % project['repo_name']
-    '''
-
-#how many repos are we tracking per project?
-for i, proj_number in enumerate(GITHUB_REPOS):
-	print("Tracking "+str(len(proj_number))+ " Project#"+str(i)+" files")
-
-def get_projects():
 	"""
-	Gets all of the project repos listed in GITHUB_REPOS and puts them into a projects list.
-	Each element of the projects list represents one of the 5 projects. Each element is a list itself,
-	with each element being one of the teams for the project. The element is a dict that has relevant
-	info about each member.
+	Gets all commits for the repo
+	:param project:
+	:return: None
 	"""
-	projects = []  # 0 will be all project 1 repos, etc
+	repo_name = project['repo_name']
 
-	for i, proj_number in enumerate(GITHUB_REPOS):
-		projects.append([])  # create empty list to hold this project's repos
-		#for each repo we have
-		for project in proj_number:
-			# create dict to hold all current project information that we gather
-			temp_proj = {'url': 'http://www.github.com/' + project,
-					 'owner': '',
-					 'team_members': {},
-					 'total_commits': 0}
+	print "Getting Commits for: %s" % repo_name
+	
+	project['commit_count'] = 0
 
-			total_commits = 0
-			print("Fetching commits for project: "+(''.join(project)))
-			#get commit log
-			next_url = 'https://api.github.com/repos/'+(''.join(project))+'/commits'
-			while next_url:
+	next_url = 'https://api.github.com/repos/'+(''.join(repo_name))+'/commits'
+	while next_url:
+		r = requests.get(next_url, auth=('cs399contributions', 'contributions399'))
+		#convert JSON string into Python nested dictionary/list
+		json_commits = json.loads(r.text)
 
-				r = requests.get(next_url, auth=('cs399contributions', 'contributions399'))
-				#convert JSON string into Python nested dictionary/list
-				json_contrib = json.loads(r.text)
+		for commit in json_commits:
+			project['commit_count'] += 1
+			contrib_info = {} # used for adding a new contributor with add_contrib() method
 
-				for commit in json_contrib:
-					total_commits += 1
-					team_member = commit['commit']['author']['name']
-					if team_member not in temp_proj['team_members']:  # check if member has been added
-						temp_proj['team_members'][team_member] = {}   # add member
-						team_member = temp_proj['team_members'][team_member] # team_member's actual name
-						if not commit['author']:  # not a "contributor" so they don't have author info
-							team_member['username'] = None
-							team_member['email'] = None
-							team_member['avatar_url'] = None 		  
-						else: 		      # otherwise get their info
-							team_member['username'] = commit['author']['login']
-							team_member['email'] = commit['commit']['author']['email']
-							team_member['avatar_url'] = commit['author']['avatar_url']
-						
-						team_member['commits'] = 1  # this is the first commit from them
-					
-					else:				# member already existed in team_members    
-						temp_proj['team_members'][team_member]['commits'] += 1
+			# get contributor's info 
+			if not commit['author']:  # not a listed github user so they don't have author info
+				contrib_info['name'] = commit['commit']['author']['name']
+				contrib_info['username'] = contrib_info['name']
+				contrib_info['email'] = None
+				contrib_info['avatar_url'] = None 
+		  
+			else: 	# they are a listed github user so get their info
+				contrib_info['name'] = commit['commit']['author']['name']
+				contrib_info['username'] = commit['author']['login']
+				contrib_info['email'] = commit['commit']['author']['email']
+				contrib_info['avatar_url'] = commit['author']['avatar_url']
 
-				# api only gets 30 commits at a time, so need to get the next page link
-				if 'next' in r.links:
-					next_url = r.links['next']['url']
-				else:
-					next_url = ''
+			###################################################################
+			# TODO add contributor to datastore with add_contrib() method
+			###################################################################
+			#add_contrib(contrib_info)
+	
+			# store the information for the commit
+			commit_info = {}  # the commit's project repo_name, username, and commit url
+			
+			# store contrib username
+			if not contrib_info['username']:  # contrib. didn't have a username, only a name
+				commit_info['username'] = contrib_info['name']
+			
+			else:  # contrib had a username
+				commit_info['username'] = contrib_info['username']
+								
+			# store commit project repo_name
+			commit_info['repo_name'] = repo_name
+			# store commit api url
+			commit_info['commit_api_url'] = commit['commit']['url']
+			
+			# send data to be processed and saved to datastore
+			save_commit(commit_info)
 
-			print("Total commits: " + str(total_commits)) 
-			temp_proj['total_commits'] = total_commits
-			#print(temp_proj)
-			projects[i].append(temp_proj) # add the current repo info to the projects list
-	return projects
+		# api only gets 30 commits at a time, so need to get the next page link
+		if 'next' in r.links:
+			next_url = r.links['next']['url']
 
-def write_to_file(data):
+		else:
+			next_url = ''
+	return project['commit_count']
+
+def add_contrib(data):
+	##########################################################
+	# TODO put student in datastore based on "data" dictionary
+	##########################################################
+	pass
+
+
+def get_commit_info(commit_url):
 	"""
-	Takes the projects list generated by get_projects() and prints it to a file.
-	:param data: All of the project data that the get_projects() method returns
+	:param commit_url: api url that holds the commit data
 	"""
-	f = open('collected_data.txt', 'w')
+	r = requests.get(commit_url, auth=('cs399contributions', 'contributions399'))
+	commit_json_data = json.loads(r.text)
+	return commit_json_data
+
+
+def save_commit(data):
+	"""
+	data should be in format {'repo_name': '', 'username': '', 'commit_api_url': ''}
+	"""
+	###########################################################
+	# TODO actually save the commit to the datastore
+	###########################################################
+
+	# get detailed commit information in python dict form	
+	#commit_data = get_commit_info(data['commit_api_url'])
+	# just save this to global variable for now to test it
+	ALL_REPOS_COMMITS_URLS.append(data)
+	print("Added commit for project " + data['repo_name'] + " made by " + data['username'])
+	
+	# TODO save the new commit to the datastore    
+	return True
+
+
+def write_to_file(data, filename):
+	f = open(filename, 'w')
 	for i, proj in enumerate(data):
-		f.write("====================== Project %d ================================\n" % (i+1))
-		f.write("\n")
 		for repo in proj:
 			f.write(str(repo))
 			f.write(", \n")
@@ -158,26 +183,15 @@ def write_to_file(data):
 	f.close()
 
 
-
-'''
-    # TODO Parse commit
-    # Add students found
-
-    # Increment commit counter
-    project['commit_count'] += 1
-
-
-def save_commit(data):
-    pass
-
-
 if __name__ == "__main__":
-    for i, project_list in enumerate(GITHUB_REPOS):
-        # projects are not zero based
-        project_number = i + 1
+	for i, project_list in enumerate(GITHUB_REPOS):
+		print("Tracking " + str(len(project_list)) + " Project#" + str(i) + " files")
 
-        print "*** Getting Repos for Project #%d ***" % project_number
+		# projects are not zero based
+		project_number = i + 1
+		
+		print("*** Getting Repos for Project #%d ***" % project_number)
 
-        for project in project_list:
-            get_repo(project, project_number)
-'''
+		for project in project_list:
+			get_repo(project, project_number)
+
