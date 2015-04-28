9064111d9efb6eacba702e36fd8fc66737f628e2
Michael Gilbert <drumz4jah@yahoo.com>
2015-04-27 17:22:08 -0700

    Put links on Projects pages/ get owner info

--- contributions/views/projects.py
+++ contributions/views/projects.py
@@ -1,6 +1,7 @@
 from contributions.views import Request
 from contributions.models.project import Project
 from contributions.models.commit import Commit
+from contributions.models.contributor import Contributor
 import logging
 
 class ProjectsPage(Request):
@@ -8,6 +9,7 @@ class ProjectsPage(Request):
     def get(self, project_id=None):	
     	# put 0 in the url for this view to get this part to propogate all the commit_counts
        	if project_id is not None:
+       		owner = ""
        		# this will query all commits for each project and put commit counts in db
         	if int(project_id) == 0:
         		qry = Project.query()
@@ -22,8 +24,17 @@ class ProjectsPage(Request):
 
 	        	if project is None:
 	        		raise Exception("None Type Project was returned --> id was " + str(project_id))
-
-	        self.render('projects_single.html', {"project": project})
+	        	else: # get the owner's account to pass to the template
+	        		owner = Contributor.query().filter(Contributor.username == project.owner).fetch()
+	        		if len(owner) > 1:
+	        			raise Exception("More than one owner returned! " + str([o.username for o in owner]))
+	        		elif len(owner) == 0:
+	        			raise Exception("No owner was found! ")
+	        		else:
+	        			owner = owner[0]  # get rid of the list 	        			
+	        
+	        self.render('projects_single.html', {"project": project, "owner": owner})
+	        
         else:
         	qry = Project.query()
         	projects = qry.fetch()
