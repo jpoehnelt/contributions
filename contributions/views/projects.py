b2fedc797caf296b1040b0fb0b4a89e0e900da44
Michael Gilbert <drumz4jah@yahoo.com>
2015-04-30 20:02:12 -0700

    Pass login url and user to Projects template

--- contributions/views/projects.py
+++ contributions/views/projects.py
@@ -6,36 +6,41 @@ import logging
 
 class ProjectsPage(Request):
 
-    def get(self, project_id=None):	
-    	# put 0 in the url for this view to get this part to propogate all the commit_counts
-       	if project_id is not None:
-       		owner = ""
-       		# this will query all commits for each project and put commit counts in db
-        	if int(project_id) == 0:
-        		qry = Project.query()
-	    		projects = qry.fetch()
-	    		for project in projects:
-	    			count = Commit.query().filter(Commit.project == project.key).count()
-	    			project.commit_count = count
-	    			project.put()
+	def get(self, project_id=None):
+		# put 0 in the url for this view to get this part to propogate all the commit_counts
+		if project_id is not None:
+			owner = ""
+			# this will query all commits for each project and put commit counts in db
+			if int(project_id) == 0:
+				qry = Project.query()
+				projects = qry.fetch()
+				for project in projects:
+					count = Commit.query().filter(Commit.project == project.key).count()
+					project.commit_count = count
+					project.put()
 
-	    	else:  # specific project is selected
-        		project = Project.get_by_id(int(project_id))
+			else:  # specific project is selected
+				project = Project.get_by_id(int(project_id))
 
-	        	if project is None:
-	        		raise Exception("None Type Project was returned --> id was " + str(project_id))
-	        	else: # get the owner's account to pass to the template
-	        		owner = Contributor.query().filter(Contributor.username == project.owner).fetch()
-	        		if len(owner) > 1:
-	        			raise Exception("More than one owner returned! " + str([o.username for o in owner]))
-	        		elif len(owner) == 0:
-	        			raise Exception("No owner was found! ")
-	        		else:
-	        			owner = owner[0]  # get rid of the list 	        			
-	        
-	        self.render('projects_single.html', {"project": project, "owner": owner})
-	        
-        else:
-        	qry = Project.query()
-        	projects = qry.fetch()
-        	self.render('projects_all.html', {"projects": projects})
+				if project is None:
+					raise Exception("None Type Project was returned --> id was " + str(project_id))
+				else: # get the owner's account to pass to the template
+					owner = Contributor.query().filter(Contributor.username == project.owner).fetch()
+					if len(owner) > 1:
+						raise Exception("More than one owner returned! " + str([o.username for o in owner]))
+					elif len(owner) == 0:
+						raise Exception("No owner was found! ")
+					else:
+						owner = owner[0]  # get rid of the list
+
+			self.render('projects_single.html', {"project": project,
+                                                 "owner": owner,
+                                                 "user": self.user,
+                                                 "login_url": self.login_url})
+
+		else:
+			qry = Project.query()
+			projects = qry.fetch()
+			self.render('projects_all.html', {"projects": projects,
+                                              "user": self.user,
+                                              "login_url": self.login_url})
