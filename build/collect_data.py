1c9882c53b69a8e28cc063e4c07d02459b4e35c0
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-04-30 18:43:57 -0700

    dylan changed his github username

--- build/collect_data.py
+++ build/collect_data.py
@@ -28,7 +28,7 @@ GITHUB_REPOS = [
     ['Adam-Thomas/Band-Proj1', 'alecdavidson/joyful-waves', 'AustinAbhari/CS399_TheBand',
      'c1phr/cs399_band', 'cmh553/theBand', 'DylanGrayson/CS399_TheBand', 'itj3/bandPage',
      'justinwp/cs399-band', 'kbullins/CS399Band', 'lf237/band', 'lmk243/BandProject',
-     'sk367/cs399_band', 'smashfligaeta/CS399_TheBand'],
+     'sk367/cs399_band'],
     # theatre
     ['sk367/cs399_Theater', 'Adam-Thomas/Theatre-Proj2', 'brandonparee/theTheatre',
      'c1phr/cs399_theatre', 'dukeayers/cs399_theater', 'FlintyFalcon/Project2',
@@ -37,11 +37,11 @@ GITHUB_REPOS = [
     #agency
     ['alexlanza/cs399_agency', 'brandonparee/theAgency', 'dukeayers/cs399_agency',
      'DylanGrayson/Agency_Proj3', 'lf237/agency', 'kyleamcginn88/Agency', 'vtsyms/CS399Agency',
-     'yourbuddyconner/cs399-agency', 'c1phr/cs399_agency', 'smashfligaeta/Agency_Proj3'],
+     'yourbuddyconner/cs399-agency', 'c1phr/cs399_agency'],
     #social
     ['cmh553/cs399_social', 'coop741/CS399Social', 'c1phr/cs399_social',
      'DylanGrayson/social_proj4', 'dukeayers/cs399_SocialMedia', 'ErinBailey/cs399-social',
-     'yourbuddyconner/cs399-social', 'bsaganey/GoingSocial', 'helikaon00/Social', 'smashfligaeta/social_proj4'],
+     'yourbuddyconner/cs399-social', 'bsaganey/GoingSocial', 'helikaon00/Social'],
     #final
     ['breadraptor/cs399_final', 'cap377/cs399final', 'EduardoIniguez/CS399-final',
      'justinwp/contributions', 'alexlanza/CS399-Final', 'DylanGrayson/knobsock', 'lf237/project5',
