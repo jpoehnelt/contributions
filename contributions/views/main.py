59ed9c0fcc5ab8e09a54474f5f94c406257a992c
Michael Gilbert <drumz4jah@yahoo.com>
2015-04-30 20:19:24 -0700

    passed url and user info to home page template

--- contributions/views/main.py
+++ contributions/views/main.py
@@ -3,4 +3,4 @@ from contributions.views import Request
 
 class MainPage(Request):
     def get(self):
-        self.render('index.html')
+        self.render('index.html', {"user": self.user, "login_url": self.login_url})
